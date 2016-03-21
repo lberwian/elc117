459301504969b055ad25655a088cddc7700db274
Leonardo Berwian <leoberwian@gmail.com>
2016-03-20 23:46:49 -0300

    Added files via upload

---
+++ t2/t2.hs
@@ -0,0 +1,33 @@
+--1
+somaQuad :: Int -> Int -> Int
+somaQuad x y =(x^2)+(y^2)
+--2
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads l1 l2 = head l1 == head l2
+--3
+srStr :: [String] -> [String]
+srStr str = map ("Sr. "++) str
+--4
+espCont :: String -> Int
+espCont str =  length (filter (==' ')(str))
+--5
+calcList :: [Float] -> [Float]   
+calcList n = map (\x->3*x^2+2/x+1) n
+--6
+negList :: [Float] -> [Float]
+negList n = filter (<0) n
+--7
+umCem :: [Float] -> [Float]
+umCem n = filter (\n-> n>1 && n<100) n
+--8
+setIdade :: [Int] -> [Int]
+setIdade n = filter  (\x->2016-x> 1970) n
+--9
+numPar :: [Int] -> [Int]
+numPar n = filter even n
+--10
+charFound :: Char -> String -> Bool
+charFound c str = length (filter(==c)str) > 0
+
+
+--Deixei para última hora e as outras duas não deu tempo =[
