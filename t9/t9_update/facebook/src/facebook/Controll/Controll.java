9a8b245b866e972b7b5770c93f6716f07e1227f6
lberwian <leoberwian@gmail.com>
2016-07-07 18:49:01 -0300

    MVC set, GUI set

---
+++ t9/t9_update/facebook/src/facebook/Controll/Controll.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facebook.Controll;
+import facebook.Model.Model;
+import facebook.View.View;
+/**
+ *
+ * @author bbl
+ */
+public class Controll {
+    
+    
+    private Model model;
+    private View view;
+
+    public Controll(View view, Model model){
+        this.model = model;
+        this.view = view;
+    
+    
+    
+    
+    }
+    
+    
+    
+}
---
+++ t9/t9_update1/src/facebook/Controll/Controll.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facebook.Controll;
+import facebook.Model.Model;
+import facebook.View.View;
+/**
+ *
+ * @author bbl
+ */
+public class Controll {
+    
+    
+    private Model model;
+    private View view;
+    private String token;
+
+    public Controll(View view, Model model){
+        this.model = model;
+        this.view = view;
+        
+    
+    
+    
+    }
+    public void defToken(String token){
+    
+        
+        this.model.setToken(token);
+        
+    
+    
+    }
+    public String getToken(){
+    
+        return this.token;
+    
+    
+    
+    }
+    
+    
+    
+    
+}
